a185592ad8bbaf42bf8d1e3db4a9f7191da9575b
S t u a r t <github@sequation.com.au>
2012-09-06 11:21:06 +1000

    Generated the initial Rails 3 application for jandj

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jandj::Application.config.secret_token = 'dce92551817abfe0c1c874df1a0274db80ff90b5535a4d2cbf1eb8a649dcf719c6cd8ab11690b1f742ae71511e29d5fb27b60e089926a5de988b59e0f5f67d09'
---
+++ features/junk.feature
@@ -0,0 +1,6 @@
+Feature: Simple feature to see if things are working.
+  This feature is simply here to indicate whether the application is running
+  And configured correctly
+
+Scenario: Going to the home page.
+  Given I am on the homepage
